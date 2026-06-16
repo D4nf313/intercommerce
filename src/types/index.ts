b09783f1d446e0ea4db7895fb99e491d6eafe557
Daniel Suarez <danfecisf@gmail.com>
2026-06-15 20:51:55 -0500

    feat: add TypeScript interfaces for Product, Cart and Filters

---
+++ src/types/index.ts
@@ -0,0 +1,38 @@
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+}
+
+export interface ProductsResponse {
+  products: Product[];
+  total: number;
+  skip: number;
+  limit: number;
+}
+
+export interface CartItem extends Product {
+  quantity: number;
+}
+
+export interface CartState {
+  items: CartItem[];
+  addItem: (product: Product) => void;
+  removeItem: (productId: number) => void;
+  updateQuantity: (productId: number, quantity: number) => void;
+  clearCart: () => void;
+}
+
+export interface FilterState {
+  search: string;
+  category: string;
+  page: number;
+}
